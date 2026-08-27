7d1e799d9e53a6adef48449569bacb1f172d9290
sl-maravante <soumya.lokeshwar.m@gmail.com>
2026-08-27 10:40:25 -0700

    changing innerHTML to textContent

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ let massMetric=document.getElementById("massMetric")
 convertBtn.addEventListener("click",function(){
    const lengthInFeet=(Number(inputBtn.value)*3.281).toFixed(3)
    const lengthInMeter=(Number(inputBtn.value)/3.281).toFixed(3)
-   lengthMetric.innerHTML=`${inputBtn.value} meters = ${lengthInFeet} feet | ${inputBtn.value} feet = ${lengthInMeter} meters` 
+   lengthMetric.textContent=`${inputBtn.value} meters = ${lengthInFeet} feet | ${inputBtn.value} feet = ${lengthInMeter} meters` 
 
     const lengthInGallon=(Number(inputBtn.value)*0.264).toFixed(3)
    const lengthInLiter=(Number(inputBtn.value)/0.264).toFixed(3)
-   volumeMetric.innerHTML=`${inputBtn.value} liter = ${lengthInGallon} gallons | ${inputBtn.value} gallons = ${lengthInLiter} liters` 
+   volumeMetric.textContent=`${inputBtn.value} liter = ${lengthInGallon} gallons | ${inputBtn.value} gallons = ${lengthInLiter} liters` 
 
     const lengthInPound=(Number(inputBtn.value)*2.204).toFixed(3)
    const lengthInKg=(Number(inputBtn.value)/2.204).toFixed(3)
-   massMetric.innerHTML=`${inputBtn.value} kilograms = ${lengthInPound} pound | ${inputBtn.value} pound = ${lengthInKg} Kilograms` 
+   massMetric.textContent=`${inputBtn.value} kilograms = ${lengthInPound} pound | ${inputBtn.value} pound = ${lengthInKg} Kilograms` 
    inputBtn.value=""
 })
